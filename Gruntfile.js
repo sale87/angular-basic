8d68dfd2df94db7a880d3b5441f5bbb50c58fb9b
Sasa Ranisavljevic <sasa.ranisavljevic@gmail.com>
2014-09-26 22:50:36 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['app/**/*.js'],
+                dest: 'dist/js/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        bower_concat: {
+            all: {
+                dest: '/dist/js/vendor.js'
+            }
+        }
+    });
+
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
